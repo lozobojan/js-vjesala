96ec0be7db8a1d018fa21db5e72719ee104b54e4
Bojan Lozo <lozobojan@gmail.com>
2020-12-07 19:29:13 +0100

    Dodati komentari u kod

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// niz objekata koji predstavljaju oblast i ponudjene rijeci
 const pitanja = [
   {
     oblast: "Pogodite grad u Crnoj Gori",
@@ -18,46 +19,60 @@ const pitanja = [
       "php",
       "python",
       "cpp",
-      "csharp"
+      "csharp",
+      "kotlin"
     ]
   }
 ];
 
-let odgovor = "";
-let brGresaka = 0;
-let maksBrGresaka = 6;
-let pogodjenaSlova = [];
-let trenutnaRijec = null;
+let odgovor = ""; // rijec koju treba pogoditi
+let brGresaka = 0; // do sad napravljeno gresaka
+let maksBrGresaka = 6; // maksimalan broj dozvoljenih pokusaja
+let pokusanaSlova = []; // slova koja su do sad pokusana
+let trenutnaRijec = null; // rijec sastavljena od pogodjenih slova i od karaktera "_" za ona koja nisu poznata
 
+// iz niza biramo nasumican pojam koji treba pogoditi
 function nasumicanOdgovor(){
-  // 0 <= n < 1
+  // Math.random() vraca broj n koji je 0 <= n < 1
+  // prvo biramo nasumicno oblast
   let oblast = pitanja[Math.floor( Math.random() * pitanja.length)];
+  // onda biramo nasumicno pojam iz oblasti
   odgovor = oblast.ponudjeno[Math.floor( Math.random() * oblast.ponudjeno.length)];
+  // popunjavamo paragraf nazivom oblasti
   document.getElementById('oblastNaziv').innerHTML = oblast.oblast;
+  // kad sve zavrsimo, pozivamo metod koji od rijeci pravi string sa donjim crtama 
   zadajTrazenuRijec();
 }
 
+// metod koji za svako slovo zadate rijeci dodijeljuje po jednu donju crtu
 function zadajTrazenuRijec(){
   let rijecTemp = "";
   for(let i = 0; i < odgovor.length; i++){
     rijecTemp += "_";
   }
+  // prikazujemo zadatu rijec na ekranu
   document.getElementById('trazenaRijec').innerHTML = rijecTemp;
 }
 
+// generisemo dugmad za izbor slova
 function generisiDugmad(){
   let abeceda = ['a','b','c','č','ć','d','dž','đ','e','f','g','h','i','j','k','l','lj','m','n','nj','o','p','r','s','š','t','u','v','z','ž'];
   let dugmadHTML = [];
+  // u niz dodajemo po dugme za svako slovo koje ima jedninstven id i metod koji se poziva na klik
   abeceda.forEach( slovo => {
     dugmadHTML.push(
       `<button id="${slovo}" class="btn btn-lg m-2 btn-primary" onClick="pokusajSlovo('${slovo}')" > ${slovo} </button>`
     );
   });
+  // prikazujemo svu dugmad na ekranu
   document.getElementById('tastatura').innerHTML = dugmadHTML.join('');
 }
 
+// metod koji se poziva kad igrac klikne neko slovo
 function pokusajSlovo(odabranoSlovo){
-  pogodjenaSlova.push(odabranoSlovo);
+  // smjesti slovo u niz pokusanih
+  pokusanaSlova.push(odabranoSlovo);
+  // postavi dugme na disabled da se ne bi moglo opet kliknuti
   document.getElementById(odabranoSlovo).setAttribute('disabled', true);
   // ako je slovo pogodjeno
   if(odgovor.indexOf(odabranoSlovo) >= 0 ){
@@ -71,27 +86,35 @@ function pokusajSlovo(odabranoSlovo){
   }
 }
 
+// metod koji rijesava situaciju kad je slovo pogodjeno
 function pogodjenoSlovo(){
-  trenutnaRijec = "";
-  let odgovorTemp = odgovor.split(''); // niz
+  trenutnaRijec = ""; // postavljam trenutnu rijec na prazan string
+  let odgovorTemp = odgovor.split(''); // niz svih slova stringa odgovor
 
+  // za svako slovo rijeci koju treba pogoditi, provjerimo je li ga korisnik vec pokusao
   odgovorTemp.forEach(slovo => {
-    if(pogodjenaSlova.indexOf(slovo) >= 0) trenutnaRijec += slovo;
+    // ako jeste, prikazujemo na ekranu
+    if(pokusanaSlova.indexOf(slovo) >= 0) trenutnaRijec += slovo;
+    // ako nije, umjesto tog slova prikazujemo donju crtu
     else trenutnaRijec += "_";
   });
+  // tako nastalu rijec prikazujemo na ekranu
   document.getElementById('trazenaRijec').innerHTML = trenutnaRijec;
 }
 
+// provjeravamo da li je korisnik pobijedio
 function provjeriPobjedu(){
   if(trenutnaRijec === odgovor){
     document.getElementById('tastatura').innerHTML = "<h2>BRAVO!!! Pobijedili ste.<h2>";
   }
 }
 
+// azuriramo broj pogresnih pokusaja na ekranu
 function azurirajGreske(){
   document.getElementById('brGresaka').innerHTML = brGresaka;
 }
 
+// provjeravamo da li je korisnik izgubio
 function provjeriPoraz(){
   if(brGresaka === maksBrGresaka){
     document.getElementById('trazenaRijec').innerHTML = `Tacan odogovor je: <b>${ odgovor }</b>`;
@@ -99,22 +122,27 @@ function provjeriPoraz(){
   }
 }
 
+// azuriramo sliku vjesala
 function azurirajSliku(){
   document.getElementById('vjesalaSlika').src = './img/'+brGresaka+'.png';
 }
 
+// metod koji se poziva na klik za ponovni pocetak igre
 function reset(){
 
-  location.reload();
+  // mozemo reload-ovati ekran
+  // location.reload();
 
-  /* brGresaka = 0;
-  pogodjenaSlova = [];
+  // ili mozemo postaviti igricu na pocetno stanje
+  brGresaka = 0;
+  pokusanaSlova = [];
   document.getElementById('vjesalaSlika').src = './img/0.png';
 
   nasumicanOdgovor();
-  azurirajGreske(); */
+  azurirajGreske();
 }
 
+// kad se ucita stranica, pozovi metode za generisanje odgovora i prikaz tastature
 nasumicanOdgovor();
 generisiDugmad();
 
